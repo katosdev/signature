fa12d2d77e397a6eddd230af171845c43b87475d
IanM <16573496+imorland@users.noreply.github.com>
2023-12-17 16:38:27 +0000

    improve test (#20)
    * include descriptive message for current failing tests
    
    * improve create, add first edit test

--- tests/integration/api/CreateSignatureTest.php
+++ tests/integration/api/CreateSignatureTest.php
@@ -14,15 +14,25 @@ class CreateSignatureTest extends TestCase
     {
         parent::setUp();
 
+        $this->extension('katosdev-signature');
+
         $this->prepareDatabase([
             'users' => [
                 $this->normalUser(),
                 ['id' => 2, 'username' => 'normal2', 'email' => 'normal2@machine.local', 'is_email_confirmed' => true],
-                ['id' => 3, 'username' => 'moderator', 'email' => 'moderator@machine.local']
+                ['id' => 3, 'username' => 'moderator', 'email' => 'moderator@machine.local'],
+                ['id' => 4, 'username' => 'normal3', 'email' => 'normal3@machine.local', 'is_email_confirmed' => true],
             ],
             'group_permission' => [
+                ['permission' => 'user.allowSignature', 'group_id' => 5],
                 ['permission' => 'user.allowSignature', 'group_id' => 4],
                 ['permission' => 'user.editSignature', 'group_id' => 4],
+            ],
+            'groups' => [
+                ['id' => 5, 'name_singular' => 'TestSig', 'name_plural' => 'TestSigs', 'color' => '#FF0000', 'icon' => 'fas fa-user'],
+            ],
+            'group_user' => [
+                ['user_id' => 4, 'group_id' => 5],
             ],
         ]);
     }
@@ -64,9 +74,9 @@ class CreateSignatureTest extends TestCase
     public function user_can_create_signature_with_permission()
     {
         $response = $this->send(
-            $this->request('PATCH', '/api/users/3',
+            $this->request('PATCH', '/api/users/4',
                 [
-                    'authenticatedAs' => 3,
+                    'authenticatedAs' => 4,
                     'json' => [
                         'data' => [
                             'attributes' => [
@@ -85,9 +95,9 @@ class CreateSignatureTest extends TestCase
         $this->assertArrayHasKey('signature', $json['data']['attributes'], 'Creating a signature failed');
         $this->assertEquals('This is my signature', $json['data']['attributes']['signature']);
 
-        $user = User::find(2);
+        $user = User::find(4);
 
-        $this->assertEquals('This is my signature', $user->signature);
+        $this->assertEquals('<t>This is my signature</t>', $user->signature);
     }
 
     /**
@@ -96,9 +106,9 @@ class CreateSignatureTest extends TestCase
     public function user_cannot_create_signature_for_other_user()
     {
         $response = $this->send(
-            $this->request('PATCH', '/api/users/3',
+            $this->request('PATCH', '/api/users/2',
                 [
-                    'authenticatedAs' => 2,
+                    'authenticatedAs' => 4,
                     'json' => [
                         'data' => [
                             'attributes' => [
@@ -112,11 +122,7 @@ class CreateSignatureTest extends TestCase
 
         $this->assertEquals(403, $response->getStatusCode(), 'Expecting a permission denied 403');
 
-        $json = json_decode($response->getBody()->getContents(), true);
-
-        $this->assertNull($json['data']['attributes']['signature']);
-
-        $user = User::find(3);
+        $user = User::find(2);
 
         $this->assertNull($user->signature);
     }
