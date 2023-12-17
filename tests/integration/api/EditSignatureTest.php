f7b16c708e60d6dd22e878a681cc5c01fa40c8f9
IanM <ian@morland.me>
2023-12-17 17:04:45 +0000

    improve tests

--- tests/integration/api/EditSignatureTest.php
+++ tests/integration/api/EditSignatureTest.php
@@ -19,19 +19,20 @@ class EditSignatureTest extends TestCase
         $this->prepareDatabase([
             'users' => [
                 $this->normalUser(),
-                ['id' => 2, 'username' => 'normal2', 'email' => 'normal2@machine.local', 'is_email_confirmed' => true, 'signature' => 'too-obscure'],
-                ['id' => 3, 'username' => 'moderator', 'email' => 'moderator@machine.local', 'signature' => 'too-obscure2'],
-                ['id' => 4, 'username' => 'normal3', 'email' => 'normal3@machine.local', 'is_email_confirmed' => true, 'signature' => 'too-obscure3'],
+                ['id' => 3, 'username' => 'normal2', 'email' => 'normal2@machine.local', 'is_email_confirmed' => true, 'signature' => 'too-obscure'],
+                ['id' => 4, 'username' => 'moderator', 'email' => 'moderator@machine.local', 'signature' => 'too-obscure2'],
+                ['id' => 5, 'username' => 'normal3', 'email' => 'normal3@machine.local', 'is_email_confirmed' => true, 'signature' => 'too-obscure3'],
             ],
             'group_permission' => [
                 ['permission' => 'user.allowSignature', 'group_id' => 5],
+                ['permission' => 'user.allowSignature', 'group_id' => 4],
                 ['permission' => 'user.editSignature', 'group_id' => 4],
             ],
             'groups' => [
                 ['id' => 5, 'name_singular' => 'TestSig', 'name_plural' => 'TestSigs', 'color' => '#FF0000', 'icon' => 'fas fa-user'],
             ],
             'group_user' => [
-                ['user_id' => 4, 'group_id' => 5],
+                ['user_id' => 5, 'group_id' => 5],
             ],
         ]);
     }
@@ -42,9 +43,9 @@ class EditSignatureTest extends TestCase
     public function user_can_edit_own_signature_when_allowed_to_have_one()
     {
         $response = $this->send(
-            $this->request('PATCH', '/api/users/4',
+            $this->request('PATCH', '/api/users/5',
                 [
-                    'authenticatedAs' => 4,
+                    'authenticatedAs' => 5,
                     'json' => [
                         'data' => [
                             'attributes' => [
@@ -62,7 +63,7 @@ class EditSignatureTest extends TestCase
 
         $this->assertEquals('This is my new signature', $json['data']['attributes']['signature']);
 
-        $user = User::find(4);
+        $user = User::find(5);
 
         $this->assertEquals('<t>This is my new signature</t>', $user->signature);
     }
