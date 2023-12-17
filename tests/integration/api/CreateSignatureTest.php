bf984dff309338b131c39502b0b8da1abe1493d5
IanM <ian@morland.me>
2023-12-17 15:31:22 +0000

    other user create

--- tests/integration/api/CreateSignatureTest.php
+++ tests/integration/api/CreateSignatureTest.php
@@ -22,6 +22,7 @@ class CreateSignatureTest extends TestCase
             ],
             'group_permission' => [
                 ['permission' => 'user.allowSignature', 'group_id' => 4],
+                ['permission' => 'user.editSignature', 'group_id' => 4],
             ],
         ]);
     }
@@ -64,6 +65,68 @@ class CreateSignatureTest extends TestCase
     {
         $response = $this->send(
             $this->request('PATCH', '/api/users/3',
+                [
+                    'authenticatedAs' => 3,
+                    'json' => [
+                        'data' => [
+                            'attributes' => [
+                                'signature' => 'This is my signature',
+                            ],
+                        ],
+                    ],
+                ]
+            )
+        );
+
+        $this->assertEquals(200, $response->getStatusCode());
+
+        $json = json_decode($response->getBody()->getContents(), true);
+
+        $this->assertEquals('This is my signature', $json['data']['attributes']['signature']);
+
+        $user = User::find(2);
+
+        $this->assertEquals('This is my signature', $user->signature);
+    }
+
+    /**
+     * @test
+     */
+    public function user_cannot_create_signature_for_other_user()
+    {
+        $response = $this->send(
+            $this->request('PATCH', '/api/users/3',
+                [
+                    'authenticatedAs' => 2,
+                    'json' => [
+                        'data' => [
+                            'attributes' => [
+                                'signature' => 'This is my signature',
+                            ],
+                        ],
+                    ],
+                ]
+            )
+        );
+
+        $this->assertEquals(403, $response->getStatusCode());
+
+        $json = json_decode($response->getBody()->getContents(), true);
+
+        $this->assertNull($json['data']['attributes']['signature']);
+
+        $user = User::find(3);
+
+        $this->assertNull($user->signature);
+    }
+
+    /**
+     * @test
+     */
+    public function user_with_permission_can_create_signature_for_other_user()
+    {
+        $response = $this->send(
+            $this->request('PATCH', '/api/users/2',
                 [
                     'authenticatedAs' => 3,
                     'json' => [
