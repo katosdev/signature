20074a2b94e9488c274963521b910036d99c32a8
IanM <ian@morland.me>
2023-12-18 07:44:25 +0000

    feat: append signature directly after post-body

--- js/src/forum/extendCommentPost.tsx
+++ js/src/forum/extendCommentPost.tsx
@@ -1,14 +1,16 @@
-import app from 'flarum/forum/app';
 import { extend } from 'flarum/common/extend';
-import type Mithril from 'mithril';
 import CommentPost from 'flarum/forum/components/CommentPost';
 import Signature from './components/Signature';
 
 export default function extendCommentPost() {
-  extend(CommentPost.prototype, 'view', function (vnode: Mithril.Vnode) {
-    if (this.attrs.post.user()) {
+  extend(CommentPost.prototype, 'content', function (content) {
+    if (this.attrs.post.user?.()) {
       if (this.attrs.post.user().signature()) {
-        vnode.children.push(<Signature user={this.attrs.post.user()} readonly={true} />);
+        content.push(
+          <div className="Post-signature">
+            <Signature user={this.attrs.post.user()} readonly={true} />
+          </div>
+        );
       }
     }
   });
