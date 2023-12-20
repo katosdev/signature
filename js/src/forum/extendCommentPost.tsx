9a6ec1d86e949e02841dafb95f71bc266d8ad151
Davide Iadeluca <davide.iadeluca@glowingblue.com>
2023-12-20 07:44:25 +0100

    feat(CommentPost): check if inline editing is allowed

--- js/src/forum/extendCommentPost.tsx
+++ js/src/forum/extendCommentPost.tsx
@@ -1,3 +1,4 @@
+import app from 'flarum/forum/app';
 import { extend } from 'flarum/common/extend';
 import CommentPost from 'flarum/forum/components/CommentPost';
 import Signature from './components/Signature';
@@ -6,9 +7,11 @@ export default function extendCommentPost() {
   extend(CommentPost.prototype, 'content', function (content) {
     if (this.attrs.post.user?.()) {
       if (this.attrs.post.user().signature()) {
+        const allowInlineEditing = app.forum.attribute('enableInlineEditing') || false;
+
         content.push(
           <div className="Post-signature">
-            <Signature user={this.attrs.post.user()} readonly={true} />
+            <Signature user={this.attrs.post.user()} readonly={!allowInlineEditing} />
           </div>
         );
       }
